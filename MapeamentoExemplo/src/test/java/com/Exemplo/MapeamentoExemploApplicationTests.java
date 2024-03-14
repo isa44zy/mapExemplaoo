5193042d018b7a845cbf4a9d3fb1a97af811d18f
isa44zy <162628718+isa44zy@users.noreply.github.com>
2024-03-14 14:32:53 -0300

    Add files via upload

---
+++ MapeamentoExemplo/src/test/java/com/Exemplo/MapeamentoExemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Exemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapeamentoExemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
